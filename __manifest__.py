1fc951fa68105890adc256386a3ad6b54febad50
Rueda2360 <alerueda2360@gmail.com>
2024-03-07 16:54:02 +0100

    intento 2

--- __manifest__.py
+++ __manifest__.py
@@ -19,21 +19,10 @@
 
         'security/ir.model.access.csv',
 
-        #Aqui distintas vistas de equipo (vistas diferentes, mismo modelo)
-        'views/liga_equipo.xml',
-        'views/liga_equipo_clasificacion.xml',
-        #Vista a un informe
-        'report/liga_equipo_clasificacion_report.xml',
-        #Aqui vista sobre los partidos
-        'views/liga_partido.xml',
-        #Añadimos un Wizard para introducir equipos
-        'wizard/liga_equipo_wizard.xml',
-        'wizard/liga_partido_wizard.xml'
+        'views/flota_vehiculo.xml'
+
         
         
     ],
-    # Fichero con data de demo si se inicializa la base de datos con "demo data" (No incluido en ejemplo)
-    # 'demo': [
-    #     'demo.xml'
-    # ],
+
 }
