ef59945f4ab06c41ac319cb3611cd0ee0c42ad94
Rueda2360 <alerueda2360@gmail.com>
2024-03-07 16:51:46 +0100

    primer try coche

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from . import liga_equipo
-from . import liga_partido
+
+from . import flota_vehiculo
 
 # Aqui indicamos que se cargara el fichero "liga_equipo.py" y "liga_partido.py"
 # Si creamos mas modelos, deben importarse en este fichero
---
+++ models/flota_vehiculo.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from datetime import timedelta
+
+from odoo import models, fields
+
+
+
+#Definimos modelo Liga Equipo, que almacenara información de cada equipo
+class FlotaVehiculo(models.Model):
+
+    #Nombre y descripcion del modelo
+    _name = 'flota.vehiculo'
+
+    _description = 'Vehiculo de la flota'
+
+    #Parametros de ordenacion por defecto
+    _order = 'marca'
+    _rec_name = 'matricula'
+    #ATRIBUTOS
+
+    marca = fields.Char('Marca',required=True)
+    matricula = fields.Char('Matricula',required=True)
+    foto = fields.Image('Image',max_width=50,max_height=50)
+    estado = fields.Char('Estado',required=True)
+    #PARA CUANDO NO HAY UN ATRIBUTO LLAMADO NAME PARA MOSTRAR NOMBRE DE UN REGISTRO
+    # https://www.odoo.com/es_ES/forum/ayuda-1/how-defined-display-name-in-custom-many2one-91657
+    
+    #Indicamos que atributo sera el que se usara para mostrar nombre.
+    #Por defecto es "name", pero si no hay un atributo que se llama name, aqui lo indicamos
+    #Aqui indicamos que se use el atributo "nombre"
+    
+
+
+    
+
+
