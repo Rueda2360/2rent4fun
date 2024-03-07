e6f49d4c62a974409465ccae15f50be13b584b8b
Rueda2360 <alerueda2360@gmail.com>
2024-03-07 17:51:25 +0100

    devolver_vehiculo()

--- models/flota_alquiler.py
+++ models/flota_alquiler.py
@@ -13,16 +13,19 @@ class FlotaAlquiler(models.Model):
     fechaInicio = fields.Date('Fecha de inicio', default=fields.Date.today(), required=True)
     fechaDevolucion = fields.Date('Fecha de devolución prevista', required=True)
 
-    disponible = fields.Boolean('Disponible', default=False)
+    #disponible = fields.Boolean('Disponible', default=False)
 
+    
     def devolver_vehiculo(self):
-        self.devuelto = True
+        self.matriculaCoche.estado="Disponible"
 
     @api.constrains('matriculaCoche')
     def estadoVehiculo(self):
         for record in self:
-            if record.matricula.estado != 'Disponible':
+            if record.matriculaCoche.estado != 'Disponible':
                 raise ValidationError('El vehículo seleccionado no está disponible para alquilar.')
+    
+                
 
     @api.constrains('fechaInicio')
     def funcionFechaPrestamo(self):
@@ -36,3 +39,10 @@ class FlotaAlquiler(models.Model):
             yesterday = fields.Date.today() - timedelta(days=1)
             if registro.fechaDevolucion and registro.fechaDevolucion < yesterday:
                 raise ValidationError('La fecha prevista de vuelta no puede ser anterior al día de ayer.')
+
+    def unlink(self):
+        #Borro el registro, que es lo que hace el metodo normalmente
+        result=super(FlotaAlquiler,self).unlink()
+        #Añado que llame a devolver_vehiculo()
+        self.devolver_vehiculo()
+        return result
